3c4784d7fa8a3f9d01aaa6ec207152f36550172c
J Fixby <github@jfixby.com>
2019-03-23 22:13:13 +0100

    move vote settings to a dedicated file

---
+++ chaincfg/vote.go
@@ -0,0 +1,125 @@
+package chaincfg
+
+// Vote describes a voting instance.  It is self-describing so that the UI can
+// be directly implemented using the fields.  Mask determines which bits can be
+// used.  Bits are enumerated and must be consecutive.  Each vote requires one
+// and only one abstain (bits = 0) and reject vote (IsNo = true).
+//
+// For example, change block height from int64 to uint64.
+// Vote {
+//	Id:          "blockheight",
+//	Description: "Change block height from int64 to uint64"
+//	Mask:        0x0006,
+//	Choices:     []Choice{
+//		{
+//			Id:          "abstain",
+//			Description: "abstain voting for change",
+//			Bits:        0x0000,
+//			IsAbstain:   true,
+//			IsNo:        false,
+//		},
+//		{
+//			Id:          "no",
+//			Description: "reject changing block height to uint64",
+//			Bits:        0x0002,
+//			IsAbstain:   false,
+//			IsNo:        false,
+//		},
+//		{
+//			Id:          "yes",
+//			Description: "accept changing block height to uint64",
+//			Bits:        0x0004,
+//			IsAbstain:   false,
+//			IsNo:        true,
+//		},
+//	},
+// }
+//
+type Vote struct {
+	// Single unique word identifying the vote.
+	Id string
+
+	// Longer description of what the vote is about.
+	Description string
+
+	// Usable bits for this vote.
+	Mask uint16
+
+	Choices []Choice
+}
+
+// Choice defines one of the possible Choices that make up a vote. The 0 value
+// in Bits indicates the default choice.  Care should be taken not to bias a
+// vote with the default choice.
+type Choice struct {
+	// Single unique word identifying vote (e.g. yes)
+	Id string
+
+	// Longer description of the vote.
+	Description string
+
+	// Bits used for this vote.
+	Bits uint16
+
+	// This is the abstain choice.  By convention this must be the 0 vote
+	// (abstain) and exist only once in the Vote.Choices array.
+	IsAbstain bool
+
+	// This coince indicates a hard No Vote.  By convention this must exist
+	// only once in the Vote.Choices array.
+	IsNo bool
+}
+
+// VoteIndex compares vote to Choice.Bits and returns the index into the
+// Choices array.  If the vote is invalid it returns -1.
+func (v *Vote) VoteIndex(vote uint16) int {
+	vote &= v.Mask
+	for k := range v.Choices {
+		if vote == v.Choices[k].Bits {
+			return k
+		}
+	}
+
+	return -1
+}
+
+const (
+	// VoteIDMaxBlockSize is the vote ID for the the maximum block size
+	// increase agenda used for the hard fork demo.
+	VoteIDMaxBlockSize = "maxblocksize"
+
+	// VoteIDSDiffAlgorithm is the vote ID for the new stake difficulty
+	// algorithm (aka ticket price) agenda defined by DCP0001.
+	VoteIDSDiffAlgorithm = "sdiffalgorithm"
+
+	// VoteIDLNSupport is the vote ID for determining if the developers
+	// should work on integrating Lightning Network support.
+	VoteIDLNSupport = "lnsupport"
+
+	// VoteIDLNFeatures is the vote ID for the agenda that introduces
+	// features useful for the Lightning Network (among other uses) defined
+	// by DCP0002 and DCP0003.
+	VoteIDLNFeatures = "lnfeatures"
+
+	// VoteIDFixLNSeqLocks is the vote ID for the agenda that corrects the
+	// sequence lock functionality needed for Lightning Network (among other
+	// uses) defined by DCP0004.
+	VoteIDFixLNSeqLocks = "fixlnseqlocks"
+)
+
+// ConsensusDeployment defines details related to a specific consensus rule
+// change that is voted in.  This is part of BIP0009.
+type ConsensusDeployment struct {
+	// Vote describes the what is being voted on and what the choices are.
+	// This is sitting in a struct in order to make merging between btcd
+	// easier.
+	Vote Vote
+
+	// StartTime is the median block time after which voting on the
+	// deployment starts.
+	StartTime uint64
+
+	// ExpireTime is the median block time after which the attempted
+	// deployment expires.
+	ExpireTime uint64
+}
